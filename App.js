0020c954ec948f01b21368fa01b51b57110ce0d0
Capital40 <redruitglo84@outlook.com>
2025-07-15 08:25:01 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SplashScreen from './screens/SplashScreen';
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import TermsScreen from './screens/TermsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Terms" component={TermsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
